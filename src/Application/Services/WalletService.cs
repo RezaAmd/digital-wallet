56c2f049e03ff208bee035d4637b7bebc0dcb893
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2021-12-16 01:52:40 +0330

    Service

---
+++ src/Application/Services/WalletService.cs
@@ -0,0 +1,49 @@
+﻿using Application.Interfaces.Context;
+using Application.Models;
+using Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class WalletService
+    {
+        #region Dependency Injection
+        private readonly IDbContext context;
+        public WalletService(IDbContext _context)
+        {
+            context = _context;
+        }
+        #endregion
+
+        public async Task<Wallet> FindByIdAsync(string id)
+        {
+            return await context.Wallets.FindAsync(id);
+        }
+
+        public async Task<Result> CreateAsync(Wallet wallet, CancellationToken cancellationToken = default)
+        {
+            await context.Wallets.AddAsync(wallet);
+            if (Convert.ToBoolean(await context.SaveChangesAsync(cancellationToken)))
+                return Result.Success;
+            return Result.Failed();
+        }
+
+        public async Task<Result> UpdateAsync(Wallet wallet, CancellationToken cancellationToken = default)
+        {
+            context.Wallets.Update(wallet);
+            if(Convert.ToBoolean(await context.SaveChangesAsync(cancellationToken)))
+                return Result.Success;
+            return Result.Failed();
+        }
+
+        public async Task<Result> DeleteAsync(Wallet wallet, CancellationToken cancellationToken = default)
+        {
+            context.Wallets.Remove(wallet);
+            if (Convert.ToBoolean(await context.SaveChangesAsync(cancellationToken)))
+                return Result.Success;
+            return Result.Failed();
+        }
+    }
+}
