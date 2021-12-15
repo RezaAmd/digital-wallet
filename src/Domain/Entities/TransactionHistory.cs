021efd7847166d4ec4379174b4d7585f4e9753a2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2021-12-15 18:04:08 +0330

    Wallet

---
+++ src/Domain/Entities/TransactionHistory.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.Identity;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class TransactionHistory
+    {
+        public string Id { get; set; }
+        public double Amount { get; set; }
+        public double Balance { get; set; }
+        public DateTime DateTime { get; set; }
+
+        [ForeignKey("Origin")]
+        public string OriginId { get; set; }
+        [ForeignKey("Destination")]
+        public string DestinationId { get; set; }
+
+        public virtual User Origin { get; set; }
+        public virtual User Destination { get; set; }
+    }
+}
---
+++ src/Domain/Entities/Wallet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Wallet
+    {
+        public string Id { get; set; }
+        public string Seed { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
