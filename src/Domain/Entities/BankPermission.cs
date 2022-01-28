1c88683de07596717dd1751eb03b8db9923751b2
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-01-29 01:00:38 +0330

    Bank Permission

--- src/Domain/Entities/BankPermission.cs
+++ src/Domain/Entities/BankPermission.cs
@@ -1,8 +1,11 @@
-﻿namespace Domain.Entities
+﻿using System;
+
+namespace Domain.Entities
 {
     public class BankPermission
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
