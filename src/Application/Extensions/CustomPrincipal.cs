70050a0c5b41840686d7664a80607524ff6d1115
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-02-15 23:22:10 +0330

    Principal extension for current user.

---
+++ src/Application/Extensions/CustomPrincipal.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Security.Claims
+{
+    public static class CustomPrincipal
+    {
+        public static string GetCurrentWalletId(this ClaimsPrincipal claims)
+        {
+            return claims.FindFirst("wallet-id").Value;
+        }
+    }
+}
