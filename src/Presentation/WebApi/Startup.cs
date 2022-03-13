c932da27bc8bb34f3d20b5b23956f1cf3d2e6cd7
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-03-14 00:49:22 +0330

    cleanup

--- src/Presentation/WebApi/Startup.cs
+++ src/Presentation/WebApi/Startup.cs
@@ -56,7 +56,7 @@ namespace WebApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
