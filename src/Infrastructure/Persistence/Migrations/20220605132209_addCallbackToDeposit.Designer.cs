c3d3f81e0ce68044df98b022a8853756e43a2224
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-06-05 20:37:42 +0430

    Add callback to "Deposit" table.

--- src/Infrastructure/Persistence/Migrations/20220605132209_addCallbackToDeposit.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20220605132209_addCallbackToDeposit.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220605132209_addCallbackToDeposit")]
+    partial class addCallbackToDeposit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Persistence/Migrations/20220605132209_addCallbackToDeposit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class addCallbackToDeposit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Callback",
+                table: "Deposits",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Callback",
+                table: "Deposits");
+        }
+    }
+}
