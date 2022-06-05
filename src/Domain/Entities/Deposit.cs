512aafb5247dccd99e9fafca892898b4fd7740cf
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-06-05 12:21:36 +0430

    Add constructor

--- src/Domain/Entities/Deposit.cs
+++ src/Domain/Entities/Deposit.cs
@@ -6,6 +6,18 @@ namespace Domain.Entities
 {
     public class Deposit : BaseEntity
     {
+        #region Constructors
+        public Deposit(double amount, string destinationId, DepositState state = DepositState.Pending, string traceId = null)
+        {
+            Id = Guid.NewGuid().ToString();
+            Amount = amount;
+            DateTime = DateTime.Now;
+            State = state;
+            TraceId = traceId;
+            DestinationId = destinationId;
+        }
+        #endregion
+
         public string Id { get; set; }
         public string TraceId { get; set; } // Transaction id, identity, identify, refence or ...
         public double Amount { get; set; }
