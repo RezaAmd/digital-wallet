a379e47adf1aa8338e1fdb17458cdb704c79a13b
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-06-14 00:27:05 +0430

    🌱 Add money value object.

---
+++ src/Domain/ValueObjects/Money.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Domain.ValueObjects
+{
+    public class Money : ValueObject
+    {
+        public Money(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException("Amout cannot be < 0");
+            Amount = amount;
+        }
+
+        public double Amount { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Amount;
+        }
+    }
+}
