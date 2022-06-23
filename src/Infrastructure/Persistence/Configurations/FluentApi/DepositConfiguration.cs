32c2efc27a153ac2d8469fbeb897f2350cb85e4b
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-06-23 21:09:25 +0430

    Comment value object

---
+++ src/Infrastructure/Persistence/Configurations/FluentApi/DepositConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.FluentApi
+{
+    public class DepositConfiguration : IEntityTypeConfiguration<Deposit>
+    {
+        public void Configure(EntityTypeBuilder<Deposit> b)
+        {
+            b.ToTable("Deposits");
+            b.HasIndex(d => d.Id)
+                .IsUnique();
+            //b.OwnsOne(d => d.Amount);
+        }
+    }
+}
