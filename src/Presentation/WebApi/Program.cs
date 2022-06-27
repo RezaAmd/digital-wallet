a0f4365f928310d4ce30aa25b406eb0f0109a3f1
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-06-27 22:06:43 +0430

    ♻️ Refactor

--- src/Presentation/WebApi/Program.cs
+++ src/Presentation/WebApi/Program.cs
@@ -49,14 +49,15 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthorization();
 app.UseCors(MyAllowSpecificOrigins);
+app.UseAuthorization();
+
+#region Custom middleware
+
+#endregion
 
 app.UseEndpoints(endpoints =>
 {
