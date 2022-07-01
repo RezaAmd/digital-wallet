84ed06ffb04cd4161b219a0265afd603c49ca1d7
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-01 23:31:50 +0430

    ♻️ Change amount typeof 'double' to 'Money'.

--- src/Infrastructure/Persistence/Migrations/20220701185609_changeDoubleToMoneyAmount.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20220701185609_changeDoubleToMoneyAmount.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220701185609_changeDoubleToMoneyAmount")]
+    partial class changeDoubleToMoneyAmount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Infrastructure/Persistence/Migrations/20220701185609_changeDoubleToMoneyAmount.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class changeDoubleToMoneyAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Wallets_DestinationId",
+                table: "Transfers");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Transfers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DestinationId",
+                table: "Transfers",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Amount_Value",
+                table: "Transfers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transfers_Id",
+                table: "Transfers",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Wallets_DestinationId",
+                table: "Transfers",
+                column: "DestinationId",
+                principalTable: "Wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Wallets_DestinationId",
+                table: "Transfers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transfers_Id",
+                table: "Transfers");
+
+            migrationBuilder.DropColumn(
+                name: "Amount_Value",
+                table: "Transfers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DestinationId",
+                table: "Transfers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "Transfers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Wallets_DestinationId",
+                table: "Transfers",
+                column: "DestinationId",
+                principalTable: "Wallets",
+                principalColumn: "Id");
+        }
+    }
+}
