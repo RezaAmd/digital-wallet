3acd96de9f82de0e07796c2cf7e97f6ba5d6a142
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2022-07-07 19:10:08 +0430

    📦 Add custom exceptions

---
+++ src/Application/Exceptions/WalletExceptions.cs
@@ -0,0 +1,18 @@
+﻿namespace DigitalWallet.Application.Exceptions;
+
+#region Insufficient Balance
+public class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException()
+        : base()
+    { }
+
+    public InsufficientBalanceException(string message)
+        : base(message)
+    { }
+
+    public InsufficientBalanceException(string message, Exception innerException)
+        : base(message, innerException)
+    { }
+}
+#endregion
