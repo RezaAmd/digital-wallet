6f0b42bb9f55202f24c773de9559af497694e720
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-05-10 22:55:58 +0330

    🌱 Add global usings.

---
+++ src/Domain/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Security.Cryptography;
+global using System.Text;
+global using DigitalWallet.Domain.Common;
---
+++ src/Infrastructure/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using DigitalWallet.Infrastructure.Consts;
+global using Microsoft.EntityFrameworkCore;
+global using System;
+global using System.Text;
+global using DigitalWallet.Infrastructure.Common.Models;
+global using DigitalWallet.Infrastructure.Common.Interfaces;
+global using DigitalWallet.Infrastructure.Common.Services;
+global using DigitalWallet.Domain.Entities;
