9ed3e01e9ccd2ca2dc7c495954d4f2d747a53014
Reza Ahmadi <rezaahmadidvlp@gmail.com>
2024-05-10 22:55:04 +0330

    🔼 Refactor and rewrite Result object.

--- src/Application/Models/Result.cs
+++ src/Application/Models/Result.cs
@@ -1,22 +1,66 @@
 ﻿namespace DigitalWallet.Application.Models
 {
-    public class Result
+    public class BaseResult
     {
-        public Result() { }
-        internal Result(bool succeeded, List<Error> errors = null)
+        public BaseResult(bool isSuccess, List<string>? messages = default)
         {
-            Succeeded = succeeded;
-            Errors = errors;
+            IsSuccess = isSuccess;
+            if (messages != null && messages.Count > 0)
+                Messages = messages;
+        }
+        public bool IsSuccess { get; private set; }
+        public List<string> Messages { get; private set; } = new();
+    }
+    public class Result : BaseResult
+    {
+        #region Ctor
+
+        public Result(bool isSuccess, List<string>? messages = default)
+            : base(isSuccess, messages) { }
+
+        #endregion
+        #region Methods
+
+        public static Result Ok()
+            => new(true, []);
+        public static Result<TData> Ok<TData>(TData? data = default)
+            => new Result<TData>(true, data, []);
+        public static Result<TData> Fail<TData>(string? error = null, TData? data = default)
+            => new Result<TData>(false, data, [error!]);
+
+        public static Result Fail(string? error = null)
+            => new(false, [error!]);
+        public static Result Fail(List<string> messages)
+            => new(false, messages);
+
+        #endregion
+        #region Implicit
+
+        public static implicit operator Result(bool isSuccess)
+            => isSuccess ? Ok() : Fail();
+
+        #endregion
+    }
+    public class Result<TData> : BaseResult
+    {
+        public TData? Data { get; set; } = default;
+
+        #region Ctor
+
+        public Result(bool isSuccess, TData? data = default, List<string>? messages = default)
+            : base(isSuccess, messages)
+        {
+            Data = data;
         }
 
-        public bool Succeeded { get; set; }
-        public List<Error> Errors { get; set; }
+        #endregion
+        #region Implicit
 
-        public static Result Success => new Result(true, new List<Error> { });
+        public static implicit operator Result(Result<TData> result)
+            => new Result(result.IsSuccess, result.Messages);
+        public static implicit operator Result<TData>(Result result)
+            => new Result<TData>(result.IsSuccess, default, result.Messages);
 
-        public static Result Failed(List<Error> errors = null)
-        {
-            return new Result(false, errors);
-        }
+        #endregion
     }
 }
